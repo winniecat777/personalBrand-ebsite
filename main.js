9c8fb63434412164f82ee67be2364c52832113c5
winniecat777 <winniecat777@gmail.com>
2024-05-31 04:25:49 +0800

    add header btn

--- main.js
+++ main.js
@@ -2,3 +2,11 @@ import './assets/scss/all.scss';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
 console.log("Hello world!");
+
+// header btn
+$('.btn').click(function() {
+  $('.btn').toggleClass('btn-active');
+  $('.hamburger-icon').toggleClass('close');
+  $('.closure').toggleClass('close');
+  $('menu').toggleClass('close');
+})
